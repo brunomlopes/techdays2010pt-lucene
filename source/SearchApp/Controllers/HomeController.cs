brunomlopes/netponto-lucene

fetched documents from index

source/SearchApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Common;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
+using SearchApp.Models;
 
 namespace SearchApp.Controllers
 {
@@ -33,8 +35,19 @@ namespace SearchApp.Controllers
 
             var numberOfResults = 10;
             var top10Results = searcher.Search(queryanalizer.Parse(query), numberOfResults);
-            
-            return View("Index");
+            var docs = new List<DocumentViewModel>();
+            foreach (var scoreDoc in top10Results.scoreDocs)
+            {
+                var document = searcher.Doc(scoreDoc.doc);
+                var title = document.GetField(Configuration.Fields.Title).StringValue();
+                var text = document.GetField(Configuration.Fields.Text).StringValue();
+                docs.Add(new DocumentViewModel(title, text));
+            }
+            return View(new SearchViewModel(docs));
         }
+
+       
+
+        
     }
 }

brunomlopes/techdays2010pt-lucene

source/SearchApp/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using Lucene.Net.Analysis.Standard;
 using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
 using Lucene.Net.Util;
+using SearchApp.Models;
+using System.Collections.Generic;
 
 namespace SearchApp.Controllers
 {
@@ -34,8 +36,19 @@ namespace SearchApp.Controllers
 
             var numberOfResults = 10;
             var top10Results = searcher.Search(queryanalizer.Parse(query), numberOfResults);
-            
-            return View("Index");
+            var docs = new List<DocumentViewModel>();
+            foreach (var scoreDoc in top10Results.scoreDocs)
+            {
+                var document = searcher.Doc(scoreDoc.doc);
+                var title = document.GetField(Configuration.Fields.Title).StringValue();
+                var text = document.GetField(Configuration.Fields.Text).StringValue();
+                docs.Add(new DocumentViewModel(title, text));
+            }
+            return View(new SearchViewModel(docs));
         }
+
+       
+
+        
     }
 }